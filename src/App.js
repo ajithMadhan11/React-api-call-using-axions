ae3ed4fb60b036381b1299012245d760f1644b17
ajithmadhan113 <ajithmadhan.dev113@gmail.com>
2021-01-21 21:35:09 +0530

    api call using axions

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Apibasics from './Apibasics';
+function App() {
+  return (
+    <div >
+
+    <Apibasics/>
+    </div>
+  );
+}
+
+export default App;
